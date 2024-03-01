c26833e44e1023e24cd6aabaa554c60f3b3efc11
Iain Emslie <iainemslie@gmail.com>
2024-03-01 12:38:54 -0500

    delete tiles feature added

--- code/editor.py
+++ code/editor.py
@@ -103,10 +103,13 @@ class Editor:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             self.pan_input(event)
             self.selection_hotkeys(event)
             self.menu_click(event)
+
             self.canvas_add()
+            self.canvas_remove()
 
     def pan_input(self, event):
         # middle mouse button pressed / released
@@ -154,6 +157,19 @@ class Editor:
 
                 self.check_neighbours(current_cell)
                 self.last_selected_cell = current_cell
+
+    def canvas_remove(self):
+        if mouse_buttons()[2] and not self.menu.rect.collidepoint(mouse_pos()):
+            if self.canvas_data:
+                current_cell = self.get_current_cell()
+                if current_cell in self.canvas_data:
+                    self.canvas_data[current_cell].remove_id(
+                        self.selection_index)
+
+                    if self.canvas_data[current_cell].is_empty:
+                        del self.canvas_data[current_cell]
+
+                    self.check_neighbours(current_cell)
 
     # drawing
     def draw_tile_lines(self):
@@ -253,6 +269,7 @@ class CanvasTile:
         self.objects = []
 
         self.add_id(tile_id)
+        self.is_empty = False
 
     def add_id(self, tile_id):
         options = {key: value['style'] for key, value in EDITOR_DATA.items()}
@@ -261,3 +278,16 @@ class CanvasTile:
             case 'water': self.has_water = True
             case 'coin': self.coin = tile_id
             case 'enemy': self.enemy = tile_id
+
+    def remove_id(self, tile_id):
+        options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+        match options[tile_id]:
+            case 'terrain': self.has_terrain = False
+            case 'water': self.has_water = False
+            case 'coin': self.coin = None
+            case 'enemy': self.enemy = None
+        self.check_content()
+
+    def check_content(self):
+        if not self.has_terrain and not self.has_water and not self.coin and not self.enemy:
+            self.is_empty = True
